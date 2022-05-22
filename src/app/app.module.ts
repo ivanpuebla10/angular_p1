8a2fa6ad7a6dea7f442fb02cfd87fb8db94774d5
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-05-22 20:07:51 +0200

    component header created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
