f0ba90f85f2eb940a300d1996b24c94286101ab4
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-07-01 14:00:14 +0200

    task reminder added

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,21 +1,40 @@
 import { Injectable } from '@angular/core';
-import { TASKS } from '../mock-tasks'
+// import { TASKS } from '../mock-tasks'
 import {Task} from '../Task'
-import {Observable, of} from 'rxjs'
+//of's import deleted cause I'm not using it 
+import {Observable} from 'rxjs'
+import {HttpClient, HttpHeaders } from '@angular/common/http'
 
+const httpOptions = {
+  headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = "http://localhost:5000/tasks"
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 //this way is using observables:
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS)
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
+    // const tasks = of(TASKS)
+    // return tasks;
   }
 //this is the other way:
 // getTasks(): Task[] {
 //   return TASKS;
 // }
+
+deleteTask(task: Task): Observable<Task> {
+  const url = `${this.apiUrl}/${task.id}`;
+  return this.http.delete<Task>(url);
+}
+
+updateTaskReminder(task: Task): Observable<Task> {
+  const url = `${this.apiUrl}/${task.id}`;
+  return this.http.put<Task>(url, task, httpOptions)
+}
 }
