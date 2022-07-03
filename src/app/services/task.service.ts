7be92cde8d2c362d0e81030616169b5167a2e57f
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-07-03 14:33:56 +0200

    add task feature added

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -37,4 +37,8 @@ updateTaskReminder(task: Task): Observable<Task> {
   const url = `${this.apiUrl}/${task.id}`;
   return this.http.put<Task>(url, task, httpOptions)
 }
+
+addTask(task: Task): Observable<Task> {
+  return this.http.post<Task>(this.apiUrl, task, httpOptions);
+}
 }
